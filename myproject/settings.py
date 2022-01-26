ca2a6181417bde9d3d4b86e76f9acb2aa180221f
AnkitaGharde <84709511+AnkitaGharde@users.noreply.github.com>
2022-01-26 22:43:47 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+-9&&boy5_9lec)hgpuxn+5avmew87mblzwkpyj_@csfe+j#1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['datalogics.herokuapp.com','www.datalogics.herokuapp.com']
+ALLOWED_HOSTS = ['engineerbro.herokuapp.com','www.engineerbro.herokuapp.com']
 
 
 # Application definition
